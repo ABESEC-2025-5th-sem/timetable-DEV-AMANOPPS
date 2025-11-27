29d518f97418dc1e5850e8fd304fa7818ef007a5
Aman kumar  chaurasia <chaurasiaaman709@gmail.com>
2025-11-27 15:38:03 +0530

    updated with post method

--- day17/index.js
+++ day17/index.js
@@ -3,12 +3,23 @@ import express from "express";
 const app = express();
 const PORT = 8800;
 
+// Body parser (POST request ke data ko read karne ke liye)
+app.use(express.json());
+
 // Test GET API (Thunder Client me test karna)
 app.get("/test", (req, res) => {
   res.json({
     message: "Your GET API is working successfully!",
     status: "OK",
     port: PORT
+  });
+});
+
+// Test POST API
+app.post("/test", (req, res) => {
+  res.json({
+    message: "Your POST API is working successfully!",
+    receivedData: req.body
   });
 });
 
